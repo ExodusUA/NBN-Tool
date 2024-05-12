f17f9838d9574da1172eda6c91c8087d88360b96
Danichok <danielreabyviktorovych@gmail.com>
2024-05-12 21:33:11 +0300

    Update correct.tsx

--- src/components/ui/correct.tsx
+++ src/components/ui/correct.tsx
@@ -16,51 +16,45 @@ export default function Correct({ correct }: CorrectProps) {
   };
   return (
     <div>
-      
       <motion.div
         initial={{ bottom: -200 }}
-        animate={{ bottom: 0 }}
-        transition={{ ease: "easeOut", duration: 0.5 }}
+        animate={{ bottom: -26 }}
+        transition={{ type: "spring", bounce: 0.50, duration: 0.6 }}
         layout
-      
-          className={cn(
-            `fixed bottom-0 w-full left-0  bg-[#FF2A57] flex z-50`,
-            {
-              "bg-green": correct,
-            }
-          )}
-        >
-          <div className="p-8 flex justify-between items-center w-full px-10">
-            <div>
-              <p
-                className={cn(
-                  ` text-[55px] text-white socialBold leading-[64px]`,
-                  {
-                    "text-black": correct,
-                  }
-                )}
-              >
-                {correct ? "correct!" : "incorrect!"}
-              </p>
-              <p
-                className={cn(`text-center text-white socialBold`, {
+        className={cn(`fixed bottom-0 w-full left-0  bg-[#FF2A57] flex z-50 pb-8`, {
+          "bg-green": correct,
+        })}
+      >
+        <div className="p-9 flex justify-between items-center w-full px-10">
+          <div>
+            <p
+              className={cn(
+                ` text-[55px] text-white socialBold leading-[64px]`,
+                {
                   "text-black": correct,
-                })}
-              >
-                Fortnite updates: Australia's data apocalypse day
-              </p>
-            </div>
-            <Button
-              onClick={() => {
-                processAnswer();
-              }}
-              variant="white"
+                }
+              )}
             >
-              Next
-            </Button>
+              {correct ? "correct!" : "incorrect!"}
+            </p>
+            <p
+              className={cn(`text-center text-white socialBold`, {
+                "text-black": correct,
+              })}
+            >
+              Fortnite updates: Australia's data apocalypse day
+            </p>
           </div>
-          </motion.div>
-      
+          <Button
+            onClick={() => {
+              processAnswer();
+            }}
+            variant="white"
+          >
+            Next
+          </Button>
+        </div>
+      </motion.div>
     </div>
   );
 }
