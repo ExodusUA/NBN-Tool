08e32ec4c3be5e710d5f90c52a98e15bf4557410
Danichok <danielreabyviktorovych@gmail.com>
2024-05-12 03:54:36 +0300

    upd

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -9,15 +9,13 @@ import { StepNumber } from "@/components/ui";
 import { ResultNumber } from "@/components/ui/result-number";
 import LogoMark from "@/components/logo-mark";
 import AddressModal from "@/components/ui/address-modal";
+import { motion } from "framer-motion";
 
 function page() {
   return (
     <div className="p-10 h-screen bg-greenQuestions relative">
       <Logo />
       <ResultNumber />
-      
-        <LogoMark mt={'portrait:bottom-[30px]'} />
-      
 
       <Image
         src={bgFull}
@@ -30,28 +28,47 @@ function page() {
         alt="bg"
       />
       <div className="portrait:mt-[340px]">
-        <p className="supreme uppercase text-[128px] font-black leading-[110px] mt-6">
-          well <br /> done
-        </p>
-        <p className="text-[28px] socialBold mt-14">
-          Are you getting the best at work?
-        </p>
-        <p className="text-[28px] socialBook mt-2 w-[600px] leading-8">
-          Ensuring optimal broadband speeds and connection types can
-          significantly enhance a business’s operational efficiency and digital
-          competitiveness.
-        </p>
-        <div className="flex items-center mt-12">
-          <Link href="/steps">
-            <button className="socialBold text-[24px] text-white checkAdressBtn bg-blue rounded-[72px] py-3 w-[360px]  text-center  active:bg-blue-200 focus:outline-none focus:ring focus:ring-blue-100 duration-200 outline-none">
-              Check your address
-            </button>
-          </Link>
-          <p className="text-[28px] socialBold ml-12 cursor-pointer active:underline duration-200">
-            Restart
+        <motion.div
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.9 }}
+        >
+          <p className="supreme uppercase text-[128px] font-black leading-[110px] mt-6">
+            well <br /> done
           </p>
-        </div>
+        </motion.div>
+        <motion.div
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.9 }}
+        >
+          <p className="text-[28px] socialBold mt-14">
+            Are you getting the best at work?
+          </p>
+          <p className="text-[28px] socialBook mt-2 w-[600px] leading-8">
+            Ensuring optimal broadband speeds and connection types can
+            significantly enhance a business’s operational efficiency and
+            digital competitiveness.
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.9 }}
+        >
+          <div className="flex items-center mt-12">
+            <Link href="/steps">
+              <button className="socialBold text-[24px] text-white checkAdressBtn bg-blue rounded-[72px] py-3 w-[360px]  text-center  active:bg-blue-200 focus:outline-none focus:ring focus:ring-blue-100 duration-200 outline-none">
+                Check your address
+              </button>
+            </Link>
+            <p className="text-[28px] socialBold ml-12 cursor-pointer active:underline duration-200">
+              Restart
+            </p>
+          </div>
+        </motion.div>
       </div>
+      <LogoMark mt={"portrait:bottom-[30px]"} />
       {/*<AddressModal />*/}
     </div>
   );
